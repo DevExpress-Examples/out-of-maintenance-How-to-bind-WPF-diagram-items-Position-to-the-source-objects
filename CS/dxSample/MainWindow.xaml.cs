e17fda078e0d4fb7da699a681b75434ac4b00e2e
DmitryRomenskiy <Dmitry.Romenskiy@DevExpress.com>
2021-08-18 12:26:30 +0300

    init

---
+++ CS/dxSample/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Linq;
+
+namespace dxSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void DiagramDataBindingBehavior_CustomLayoutItems(object sender, DevExpress.Xpf.Diagram.DiagramCustomLayoutItemsEventArgs e) {
+            e.Handled = true;
+        }
+    }
+}
---
+++ CS/dxSample/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace dxSample {
+    public class ViewModel {
+
+        public ViewModel() {
+            Items = new List<Item>();
+            for (int i = 0; i < 5; i++)
+                Items.Add(new Item { Id = i, Name = "Item " + i, Position = new Point(i * 100, i * 200) });
+            Connections = new List<Link>();
+            for (int i = 0; i < 4; i++)
+                Connections.Add(new Link { From = Items[i].Id, To = Items[i + 1].Id });
+            Connections.Add(new Link { From = Items[4].Id, To = Items[0].Id });
+        }
+
+        public List<Link> Connections { get; set; }
+        public List<Item> Items { get; set; }
+    }
+
+    public class Item : BindableBase {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Point Position { get => GetProperty(() => Position); set => SetProperty(() => Position, value); }
+    }
+
+    public class Link {
+        public object From { get; set; }
+        public object To { get; set; }
+    }
+}
